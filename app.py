a535347348df581bb7d65baf75aeb35578b4d345
Krishna <87417611+krishna0223@users.noreply.github.com>
2025-10-28 14:06:56 +0530

    Update frontend url

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from collections import deque
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "https://gesture-sense-frontend.vercel.app/"}})
 
 # ========================================================================
 # CONFIGURATION
@@ -129,5 +129,6 @@ def predict():
 # ========================================================================
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
 
 
